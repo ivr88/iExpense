7eec57220456d97bd3f12d9d68d50747600336a6
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-11-27 19:49:12 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Вадим Исламов on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
